8020caac575ac7c75f3379e25ea73f356e6d79e8
Lekshmi Krishnan R <lekshmibindhu08@gmail.com>
2025-10-17 17:28:50 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -26,3 +26,11 @@ with st.form("Iris app form"):
         species_map = {0: "Iris-setosa", 1: "Iris-versicolor", 2: "Iris-virginica"}
         predicted_class = species_map.get(int(prediction[0]), 'unknown species')
         st.success(f"Predicted class: {predicted_class}")
+        if predicted_class == "Iris-setosa":
+            st.image("https://thumbs.dreamstime.com/b/closeup-single-iris-setosa-flower-pretty-mauve-garden-228835113.jpg", caption="Iris-setosa", use_container_width=True)
+        elif predicted_class == "Iris-versicolor":
+            st.image("https://tse2.mm.bing.net/th/id/OIP.Slm0GQWYZWy5rghhQlF2aAHaHa?cb=12ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3", caption="Iris-versicolor", use_container_width=True)
+        elif predicted_class == "Iris-virginica":
+            st.image("https://thvnext.bing.com/th/id/OIP.fT4cZGpZAKUxKFHbfHY_DQHaFh?w=253&h=188&c=7&r=0&o=7&cb=12&dpr=1.3&pid=1.7&rm=3&ucfimg=1", caption="Iris-virginica", use_container_width=True)
+        else:
+            st.warning("No image available for this prediction.")
